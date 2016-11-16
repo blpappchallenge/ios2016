9538b132045a01efa1fff3091a7205fb913bf066
Patrick O'Brien <pbobrien0@gmail.com>
2016-11-16 16:42:02 -0500

    test comment

--- Demo Repository/Test this/Controller_Main.swift
+++ Demo Repository/Test this/Controller_Main.swift
@@ -19,7 +19,7 @@ class Controller_Main: UIViewController, UICollectionViewDataSource, UICollectio
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //
+        // test comments.
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
