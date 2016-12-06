36cf3a88e76d1b0a891c9780a68ba77972e8187b
Matt Wylder <matt.wylder@gpshopper.com>
2016-12-06 14:20:55 -0600

    added favorites wrapper

--- ISDA/ISDA/AppDelegate.swift
+++ ISDA/ISDA/AppDelegate.swift
@@ -12,7 +12,9 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    var favorites = FavoritesPersister()
+    private (set) var services: [Service]?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
