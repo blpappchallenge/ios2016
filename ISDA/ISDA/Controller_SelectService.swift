16a8980b08e2b3af25fffc674ed1433fc2df1b10
Matt Wylder <matt.wylder@gpshopper.com>
2016-12-07 12:56:08 -0600

    added settings stuff

---
+++ ISDA/ISDA/Controller_SelectService.swift
@@ -0,0 +1,14 @@
+//
+//  Controller_SelectService.swift
+//  ISDA
+//
+//  Created by Matt Wylder on 12/7/16.
+//  Copyright © 2016 Zack. All rights reserved.
+//
+
+import Foundation
+
+class Controller_SelectService: UIViewController {
+    var completion: ((Service)->Void)?
+    
+}
