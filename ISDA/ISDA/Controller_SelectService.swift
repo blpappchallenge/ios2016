35b5f60395e05e0edb1f6cea5563d9bbad6aa01c
Matt Wylder <matt.wylder@gpshopper.com>
2016-12-08 12:44:20 -0600

    added select service vc

--- ISDA/ISDA/Controller_SelectService.swift
+++ ISDA/ISDA/Controller_SelectService.swift
@@ -8,7 +8,28 @@
 
 import Foundation
 
-class Controller_SelectService: UIViewController {
-    var completion: ((Service)->Void)?
+class Controller_SelectService: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    var completion: ((Service)->Void)?
+    var selectedService: Service?
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let service = selectedService {
+            completion?(service)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return App.services?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let service = App.services![indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "service")!
+        cell.textLabel?.text = service.name
+        return cell
+    }
 }
